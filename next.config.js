1e3163e0e5278f723f1d8e0478695b2c8ad16457
Koussay Akchi <kaskousakchi@gmail.com>
2026-05-16 13:08:36 +0100

    fix: next trailing slash

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1400, 1600, 1920, 2048, 3840],
     unoptimized: false,
