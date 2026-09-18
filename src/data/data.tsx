c86ffd17334ea09b984c403584e0958186d18604
Koussay Akchi <kaskousakchi@gmail.com>
2026-09-18 20:08:16 +0100

    chore: new resume template

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -749,7 +749,7 @@ export const certifications: Certification[] = [
     name: 'certAzureContainerApps',
     issuer: 'Microsoft',
     date: 'certDateAzureContainerApps',
-    url: 'https://learn.microsoft.com/api/credentials/share/en-us/koussayakchi/9FC89CD5F97D781C?sharingId=7C446232C661FA23',
+    url: 'https://learn.microsoft.com/api/credentials/share/en-us/koussayakchi/9FC89CD5F97D781C',
     logo: microsoftLogo,
   },
   {
